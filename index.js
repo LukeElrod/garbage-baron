d321a72ed2022d12a8b7634a52140eee9d27c1d8
Luke Elrod <luke.elrod.career@gmail.com>
2024-06-23 22:55:10 -0700

    update[dependencies]: uses fetch now

--- index.js
+++ index.js
@@ -17,18 +17,22 @@ client.on('messageCreate', (message) => {
 
 client.on('guildMemberUpdate', (oldMember, newMember) => {
     if (oldMember.nickname !== newMember.nickname) {
-        const ratRegex = /rat/i // create a case-insensitive regex
-        let role = newMember.guild.roles.cache.find(role => role.name === 'Rat')
-        if (ratRegex.test(newMember.nickname) || newMember.nickname.includes('🐀') || newMember.nickname.includes('🐭') || newMember.nickname.includes('🐁')) {
-            newMember.roles.add(role)
-            
-            const channel = newMember.guild.channels.cache.get('621976580977065995');
-            channel.send(`${newMember.user.tag} is now one of us.`);
-        }else{
-            newMember.roles.remove(role)
-            
-            const channel = newMember.guild.channels.cache.get('621976580977065995');
-            channel.send(`${newMember.user.tag} has forsaken their oath.`);
-        }
+        const ratRegex = /rat/i
+        
+        newMember.guild.roles.fetch("610572763668676629").then(role => {
+            if (ratRegex.test(newMember.nickname) || newMember.nickname.includes('🐀') || newMember.nickname.includes('🐭') || newMember.nickname.includes('🐁')) {
+                newMember.roles.add(role)
+                
+                const channel = newMember.guild.channels.cache.get('621976580977065995');
+                channel.send(`${newMember.user.tag} is now one of us.`);
+            }else{
+                newMember.roles.remove(role)
+                
+                const channel = newMember.guild.channels.cache.get('621976580977065995');
+                channel.send(`${newMember.user.tag} has forsaken their oath.`);
+            }
+        }).catch(error => {
+            console.error("rat role not found")
+        })
     }
 });
